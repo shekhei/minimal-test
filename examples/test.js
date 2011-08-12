6753ead937e465cd432dc75111ca38f66acccb9f
wong shek hei <wong_felix@yahoo.com>
2011-08-12 18:04:09 +0800

    adding repository into package.json

---
+++ examples/test.js
@@ -0,0 +1,42 @@
+var test = require('../lib/unit_tester');
+var path = require('path');
+
+test("Testing somet of the stuff")
+
+  ("Lets test that things are true", function(test){
+    test(true);
+  })
+
+  ("lets test a fasle", function(test){
+    test(false);
+  })
+
+  ("Lets ensure 1 is a number", function(test){
+    test.is.a.number(1);
+  })
+
+  ("Lets ensure 'a' is not a number", function(test){
+    test.is.a.number("a");
+  })
+
+  ("Lets ensure 'a' is a string", function(test){
+    test.is.a.string("a");
+  })
+
+  .async("if random.txt exists async(doesnt exist)", function(test){
+    path.exists(require('path').resolve('./unit_test/file.txt'),function(exists){
+      test(exists);
+      test.done();
+    });
+  })
+
+  .async("if exists.txt exists async", function(test){
+    path.exists(require('path').resolve('./exists.txt'),function(exists){
+      test(exists);
+      test.done();
+    });
+  })
+
+  ("Lets ensure that [1,1] is an array", function(test){
+    test.is.a.array([1,1]);
+  })
